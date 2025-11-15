960213e819bf9db812110dded2c56bec15ae8d89
Mariem Zouari <mariemzouari27@gmail.com>
2025-11-15 21:38:55 +0100

    fix

--- config.php
+++ config.php
@@ -1,19 +1,38 @@
 <?php
-// config.php
-class Config {
-    const DB_HOST = 'localhost';
-    const DB_NAME = 'impactable';
-    const DB_USER = 'root';
-    const DB_PASS = '';
-    
-    
-
-    public static function getBaseUrl() {
-        return self::SITE_URL;
-    }
-    
-    public static function getAssetUrl($path) {
-        return self::SITE_URL . '/assets/' . ltrim($path, '/');
+class config
+{   private static $pdo = null;
+    public static function getConnexion()
+    {
+        if (!isset(self::$pdo)) {
+            $servername="localhost";
+            $username="root";
+            $password ="";
+            $dbname="impactable";
+            try {
+                self::$pdo = new PDO("mysql:host=$servername;dbname=$dbname",
+                        $username,
+                        $password
+                   
+                );
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+               
+               
+            } catch (Exception $e) {
+                die('Erreur: ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
     }
 }
+config::getConnexion();
 ?>
+
+
+
+
+
+
+
+
+
